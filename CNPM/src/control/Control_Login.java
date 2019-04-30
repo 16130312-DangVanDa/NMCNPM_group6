d409cb9812bf91a9af67a4bd3f477ecc21f0e3f6
16130312-DangVanDa <16130312@st.hcmuaf.edu.vn>
2019-04-30 17:16:50 +0700

    Cập nhật Controller: login by account

--- CNPM/src/control/Control_Login.java
+++ CNPM/src/control/Control_Login.java
@@ -3,6 +3,8 @@ package control;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,14 +37,51 @@ public class Control_Login extends HttpServlet {
 		action = request.getParameter("action");
 		PrintWriter out = response.getWriter();// su dung print writer de in ket qua
 
+		// TH1: Login by Account
 		if (action.equals("account")) {
-			login = new LoginAccount();
+			System.out.println("here...");
+			// 2.2.6: lay du lieu gui tu client len
+			String username = request.getParameter("Username");
+			String pass = request.getParameter("Password");
 
+			System.out.println("username: " + username);
+			System.out.println("password: " + pass);
+			
+			// 2.2.7. khoi tao hình thức đăng nhập là đăng nhập với account
+			login = new LoginAccount();
+			System.out.println("khoi tao thanh cong");
+
+			// 2.2.8. phuong thuc check(username, password)
+//			String path= "D:\\Nong Lam University\\webprogramming\\CNPM2\\config\\config.properties";
+//			String path=  getServletContext().getRealPath("/config/config.properties");
+//			System.out.println("Path: "+path);
+			
+			String result = login.account.check(username, pass);
+			System.out.println("result " + result);
+
+			if (result != null) {
+				// đúng username, passowrd
+				// 2.2.8.1: Respone("Đăng nhập thành công")
+				out.println("Đăng nhập thành công");
+				out.println(result);
+			} else {
+				// 2.2.8.2: luu request de gui lai client
+				request.setAttribute("username", username);
+				request.setAttribute("password", pass);
+				request.setAttribute("login_fail", "Có cái gì đó không hợp lệ! Mời nhập lại");
+				// chuyen ve trang login
+				String url = "/login.jsp";
+				RequestDispatcher re = getServletContext().getRequestDispatcher(url);
+				re.forward(request, response);
+			}
+
+			// TH2:Login by facebook
 		} else if (action.equals("facebook")) {
 			login = new LoginFacebook();
-			//code here
+			// code here
 			// goi phuong thuc ra de thuc hien: login.facebook.method()
 
+			// TH3: Login by Gmail
 		} else if (action.equals("gmail")) {
 			login = new LoginGoogle();
 			// code here
